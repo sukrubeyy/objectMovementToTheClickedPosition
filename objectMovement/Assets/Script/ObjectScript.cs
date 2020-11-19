56e9f113ea0e944d2a83be8c75f1d331eb6d7c7b
sukrubeyy <sukru.beyy@outlook.com>
2020-11-19 15:15:17 +0300

    Codes
    i'm write movement codes

---
+++ objectMovement/Assets/Script/ObjectScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float speed = 5f;
+    Vector2 mousePOS;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        movementCodes();
+    }
+    void movementCodes()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            mousePOS = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        } 
+        rb.position = Vector3.Lerp(rb.position, mousePOS, 1);
+    }
+}
